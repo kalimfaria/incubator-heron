53a67a74ee2d856eb70b257725beda809a0d0acd
Faria Kalim <faria.kalim@gmail.com>
2018-08-16 20:56:52 -0700

    bug fix

--- examples/src/java/org/apache/heron/examples/api/S8CNS_delayed_backpressure.java
+++ examples/src/java/org/apache/heron/examples/api/S8CNS_delayed_backpressure.java
@@ -90,7 +90,7 @@ public final class S8CNS_delayed_backpressure {
     private static final int ARRAY_LENGTH = 128 * 1024;
     private static final int WORD_LENGTH = 20;
     private static final int SENTENCE_LENGTH = 10;
-    private static final long TIME = 1534468915;
+    private static final long TIME = 1534478175000;
 
     // Every sentence would be words generated randomly, split by space
     private final String[] sentences = new String[ARRAY_LENGTH];
